a37a61dfc7141e2ccfb2dffb9adc830f654e405c
Alpatiwari <145915241+Alpatiwari@users.noreply.github.com>
2025-07-17 15:57:19 +0530

    added space

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import './App.css';
 
 function HomePage({ query, setQuery, onSearch, isLoading, error, results }) {
   return (
+    
     <div className="home-container">
       <div className="search-content">
         <h1 className="logo">
